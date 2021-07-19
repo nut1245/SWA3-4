b7096d7e1f0068f17ce8b4a3b583ec157122b889
nut1245 <73591319+nut1245@users.noreply.github.com>
2021-07-19 15:54:44 +0700

    Initial commit

---
+++ src/Honda.java
@@ -0,0 +1,12 @@
+
+public class Honda extends Car{
+
+    public void HondaStart(){
+
+        CarEngine Honda_Engine = new CarEngine(); //composition
+
+        Honda_Engine.StartEngine();
+
+        }
+
+} 
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {   
+
+        Honda HondaJazz = new Honda();
+
+        HondaJazz.setColour(null);
+
+        HondaJazz.setMaxiSpeed(160);
+
+        HondaJazz.carDetails();
+
+        HondaJazz.HondaStart();
+
+    }
+
+}
